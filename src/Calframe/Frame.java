960a70f1b7a4d31a57a1e2fef6c13fda7276d177
2012034283_박준모 <jm2683@naver.com>
2016-06-19 19:44:48 +0900

    Junmo 3

--- src/Calframe/Frame.java
+++ src/Calframe/Frame.java
@@ -1,40 +1,43 @@
 package Calframe;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Frame extends JFrame   {
+public class Frame extends JFrame implements ActionListener  {
 	
 	protected JButton[] button;
 	protected JTextField field;
 	protected JPanel panel;
 	protected String[] labels = {"7","8","9","C","4","5","6","*","1","2","3","-" ,"0","/","+","=" };
 	
-	protected String first = ""; //누적값을 받음
+	// ===================기능구현부분 준모3==================
+	private double result = 0;
+	private String operator = "=";
+	private boolean startOfNumber = true;
+	// ===================기능구현부분 준모3==================
 	
 	public Frame()
 	{
+
 		setSize(300,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Calculator");
 		
-		
+		// ===================기능구현부분 준모3==================
 		JTextField field = new JTextField(35);
-		field.setText("0");
+		field.setText("0.0");
 		field.setEnabled(false);
-
+		// ===================기능구현부분 준모3==================
 		
 		
 		panel = new JPanel();
-		
 		panel.setLayout(new GridLayout(0,4,3,3));
 		button = new JButton[25];
 		
@@ -57,6 +60,45 @@ public class Frame extends JFrame   {
 		pack();
 		
 	}
+	
+	// ======================기능 구현 부분 ===============
+	
+	public void actionPerformed(ActionEvent e){
+		String command = e.getActionCommand();
+		if(command.charAt(0) == 'C'){
+			startOfNumber = true;
+			result = 0;
+			operator = "=";
+			field.setText(0);
+		}
+		else if (command.charAt(0) >= '0' && command.charAt(0) <= '9'||
+				command.equals(".")){
+			if(startOfNumber == true)
+				field.setText(command);
+			else
+				field.setText(field.getText() + command);
+			startOfNumber = false;
+		}
+		else{
+			if(startOfNumber){
+				if(command.equals("-")){
+					field.setText(command);
+					startOfNumber = false;
+				}
+				else
+					operator = command;
+			}
+			else{
+				double x = Double.parseDouble(field.getText());
+				Calculate(x);
+				operator = command;
+				startOfNumber = true;
+			
+			}
+		}
+		
+	}
+	// ================기능구현부분==================================
 	
 	public static void main(String args[])
 	{
