1f4a2b20c68aea75a203728b2f7d9803f8f8d0ff
Java2-2011034174 <librarian17@naver.com>
2016-06-19 19:07:40 +0900

    Up

--- src/Calframe/Frame.java
+++ src/Calframe/Frame.java
@@ -1,44 +1,47 @@
 package Calframe;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Frame extends JFrame   {
 	
 	protected JButton[] button;
-	protected JTextField field;
+	protected JTextField NorhtField,CenterField;
 	protected JPanel panel;
-	protected String[] labels = {"7","8","9","C","4","5","6","*","1","2","3","-" ,"0","/","+","=" };
+	protected JPanel Textpanel;
+	protected String[] labels = {"7","8","9","C","4","5","6","*","1","2","3","-" ,"0","/","+","=","enter"," "," "," "," " };
 	public Frame()
 	{
 		setSize(300,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Calculator");
+		//TextBlistener
 		
-		
-		JTextField field = new JTextField(35);
-		field.setText("0");
-		field.setEnabled(false);
-
+		JTextField NorthField = new JTextField(35);
+		NorthField.setText("0");
+		NorthField.setEnabled(false);
+		JTextField CenterField = new JTextField(35);
+		CenterField.setText("0");
+		CenterField.setEnabled(false);
+	
 		
 		
 		panel = new JPanel();
-		
+		Textpanel = new JPanel();
 		panel.setLayout(new GridLayout(0,4,3,3));
 		button = new JButton[25];
 		
 		int i =0;
 		for (int r= 0; r<4; r++)
 		{
-			for (int c = 0; c<4; c++)
+			for (int c = 0; c<5; c++)
 			{
 				button[i] = new JButton(labels[i]);
 				panel.add(button[i]);
@@ -48,13 +51,30 @@ public class Frame extends JFrame   {
 			
 		}
 		
-		add(field,BorderLayout.NORTH);
-		add(panel,BorderLayout.CENTER);
+		Textpanel.setLayout(new BorderLayout());    		// 레이아웃 설정.
+		Textpanel.add(BorderLayout.NORTH,NorthField);   // 패널에 텍스트필드를 두개 붙임.
+		Textpanel.add(BorderLayout.CENTER,CenterField);
+		
+		add(BorderLayout.NORTH,Textpanel); 
+		add(BorderLayout.CENTER,panel);
+		
+		
+		
 		setVisible(true);
 		pack();
 		
 	}
 	
+	protected class Buttonlistener implements ActionListener
+	{
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource() == button)
+		{
+			
+		}
+	}
+	}
 	public static void main(String args[])
 	{
 		Frame s = new Frame();
